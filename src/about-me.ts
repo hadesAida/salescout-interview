32ec830e02c227263c66153de576c46756c3a55d
hadesAida <114656337+hadesAida@users.noreply.github.com>
2025-01-16 19:57:15 +0500

    Update about-me.ts

--- src/about-me.ts
+++ src/about-me.ts
@@ -1,6 +1,12 @@
 export const contactInfo = {
-    name: 'Имя Фамилия', //Укажите здесь имя
-    phoneNumber: '+77713011111', //Укажите здесь свой номер телефона
-    email: 'yourmail@gmail.com', //Укажите здесь свой email
-    bio: '' //Напишите сопроводительное письмо(от 50 символов!!!)
+    name: 'Aida Madraimova', //Укажите здесь имя
+    phoneNumber: '+77768289027', //Укажите здесь свой номер телефона
+    email: 'kkmaida@bk.ru', //Укажите здесь свой email
+    bio: 'I am writing to express my interest in the intern position. As a current student at Kazakh British Technical University pursuing a Bachelor's degree in Computer Science, I have developed a strong foundation in full-stack development, with hands-on experience in Python, Swift, C++, Java, HTML, and JavaScript. I am eager to apply my skills and contribute to your team.
+
+Throughout my studies, I have gained proficiency in front-end frameworks like React and in state management using Redux and Context API. I have also developed RESTful APIs and worked with SQL databases. My passion for building user-centered applications, along with my enthusiasm for learning new technologies, makes me an ideal fit for this role.
+
+I am excited about the opportunity to contribute to your team and further develop my skills in a collaborative environment. I am confident that my background in full-stack development and my eagerness to continue learning will allow me to make a valuable contribution.
+
+Thank you for considering my application. I look forward to the possibility of discussing how I can contribute in more detail.' //Напишите сопроводительное письмо(от 50 символов!!!)
 }  
